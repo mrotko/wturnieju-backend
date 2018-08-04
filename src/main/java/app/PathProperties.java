f44811a16f66a9682db90b763d75049c3615ac8f
Michał Rotko <michalr835@gmail.com>
2018-08-04 22:28:17 +0200

    hello world

---
+++ src/main/java/app/PathProperties.java
@@ -0,0 +1,23 @@
+package app;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix = "path")
+@Getter
+@Setter
+@Validated
+public class PathProperties {
+
+    private String serverUrl;
+
+    private String frontendUrl;
+
+    private String apiUrl;
+
+}
---
+++ src/main/java/app/helloworld/HelloWorldController.java
@@ -0,0 +1,76 @@
+package app.helloworld;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+    private HelloWorldRepository helloWorldRepository;
+
+    public HelloWorldController(HelloWorldRepository helloWorldRepository) {
+        this.helloWorldRepository = helloWorldRepository;
+    }
+
+    @PostMapping(path = "message")
+    public HelloWorldMessage addMessage(@RequestBody Map<String, String> payload) {
+        if (payload.containsKey("message")) {
+            return helloWorldRepository.insert(HelloWorldMessage.builder().message(payload.get("message")).build());
+        }
+        return null;
+    }
+
+    @GetMapping(path = "message/{id}")
+    public HelloWorldMessage getMessage(@PathVariable("id") String id) {
+        return helloWorldRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping(path = "/message")
+    public List<HelloWorldMessage> getMessages() {
+        return helloWorldRepository.findAll();
+    }
+
+    @DeleteMapping(path = "message/{id}")
+    public HelloWorldMessage deleteMessage(@PathVariable("id") String id) {
+        HelloWorldMessage helloWorldMessage = helloWorldRepository.findById(id).orElse(null);
+        if (helloWorldMessage != null) {
+            helloWorldRepository.deleteById(id);
+            return helloWorldMessage;
+        }
+        return null;
+    }
+
+    @DeleteMapping(path = "message")
+    public List<HelloWorldMessage> deleteMessages(@RequestHeader(value = "data") String data) {
+        List<String> ids = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            ids = Stream.of(mapper.readValue(data, String[].class)).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<HelloWorldMessage> messages = new ArrayList<>();
+        ids.forEach(id -> helloWorldRepository.findById(id).ifPresent(helloWorldMessage -> {
+            messages.add(helloWorldMessage);
+            helloWorldRepository.delete(helloWorldMessage);
+        }));
+        return messages;
+    }
+}
