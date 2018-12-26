24a92aa0ae350f50b2bfd2043da96ad67042822c
Michał Rotko <michalr835@gmail.com>
2018-12-26 12:41:20 +0100

    fix to string do generowania tokena

--- src/main/java/pl/wturnieju/model/ChangeEmailVerificationToken.java
+++ src/main/java/pl/wturnieju/model/ChangeEmailVerificationToken.java
@@ -1,8 +1,10 @@
 package pl.wturnieju.model;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString(callSuper = true)
 public class ChangeEmailVerificationToken extends VerificationToken {
 
     private String oldEmail;
--- src/main/java/pl/wturnieju/model/NewAccountVerificationToken.java
+++ src/main/java/pl/wturnieju/model/NewAccountVerificationToken.java
@@ -1,8 +1,10 @@
 package pl.wturnieju.model;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString(callSuper = true)
 public class NewAccountVerificationToken extends VerificationToken {
 
     private String email;
