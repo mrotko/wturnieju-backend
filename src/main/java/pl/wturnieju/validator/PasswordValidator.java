92c41c92aa31fc8848203bf0f8023a88151efd17
Michał Rotko <michalr835@gmail.com>
2019-01-31 00:38:49 +0100

    pattern hasła bez znaków specjalnych

--- src/main/java/pl/wturnieju/validator/PasswordValidator.java
+++ src/main/java/pl/wturnieju/validator/PasswordValidator.java
@@ -4,7 +4,9 @@ public class PasswordValidator extends PatternValidator {
 
     public static final String BIG_LETTER_NUMBER_SPECIAL_8LEN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%,.]).{8,})";
 
+    public static final String BIG_LETTER_NUMBER_8LEN = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d].{8,}$";
+
     public PasswordValidator() {
-        super(BIG_LETTER_NUMBER_SPECIAL_8LEN);
+        super(BIG_LETTER_NUMBER_8LEN);
     }
 }
