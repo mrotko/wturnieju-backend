4353ed5096b9e43a1bf2ad4295ec5916bdce8d43
Michał Rotko <michalr835@gmail.com>
2019-02-04 18:39:28 +0100

    fix

--- src/main/java/pl/wturnieju/config/tournament/creator/TournamentCreatorConfiguration.java
+++ src/main/java/pl/wturnieju/config/tournament/creator/TournamentCreatorConfiguration.java
@@ -1,30 +1,35 @@
 package pl.wturnieju.config.tournament.creator;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.util.ResourceUtils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @Getter
+@RequiredArgsConstructor
 public class TournamentCreatorConfiguration {
 
     private TournamentCreatorConfigurationData tournamentCreatorConfigurationData;
 
+    private final ApplicationContext context;
+
     @PostConstruct
     private void init() {
         try {
-            var file = ResourceUtils.getFile("classpath*:config/tournament-creator.json");
+            InputStream is = context.getResource("classpath:config/tournament-creator.json").getInputStream();
             var objectMapper = new ObjectMapper();
-            tournamentCreatorConfigurationData = objectMapper.readValue(file,
+            tournamentCreatorConfigurationData = objectMapper.readValue(is,
                     new TypeReference<TournamentCreatorConfigurationData>() {
                     });
         } catch (IOException e) {
--- src/main/java/pl/wturnieju/config/tournament/scoring/ScoringConfiguration.java
+++ src/main/java/pl/wturnieju/config/tournament/scoring/ScoringConfiguration.java
@@ -1,36 +1,41 @@
 package pl.wturnieju.config.tournament.scoring;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.util.ResourceUtils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import pl.wturnieju.model.CompetitionType;
 import pl.wturnieju.model.TournamentSystemType;
 import pl.wturnieju.tournament.GameResultType;
 
 @Configuration
 @Getter
+@RequiredArgsConstructor
 public class ScoringConfiguration {
 
     private List<Competition> competitions;
 
+    private final ApplicationContext context;
+
     @PostConstruct
     private void init() {
         try {
-            var file = ResourceUtils.getFile("classpath*:config/scoring.json");
+            InputStream is = context.getResource("classpath:config/scoring.json").getInputStream();
             var objectMapper = new ObjectMapper();
-            competitions = objectMapper.readValue(file, new TypeReference<List<Competition>>() {
+            competitions = objectMapper.readValue(is, new TypeReference<List<Competition>>() {
             });
         } catch (IOException e) {
             throw new ResourceNotFoundException(e.getMessage());
