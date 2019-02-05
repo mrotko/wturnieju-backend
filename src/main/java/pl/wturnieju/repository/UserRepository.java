ea9e44ab49a04cdd1ff854766017aae6a4cc9e47
Michał Rotko <michalr835@gmail.com>
2019-02-05 19:10:32 +0100

    fix - ignorowanie wielkości liter przy wyszukiwaniu użytkownika

--- src/main/java/pl/wturnieju/repository/UserRepository.java
+++ src/main/java/pl/wturnieju/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pl.wturnieju.model.User;
@@ -11,6 +12,7 @@ import pl.wturnieju.model.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
+    @Query("{username: {$regex: ?0, $options: 'i'}}")
     Optional<User> findByUsername(String username);
 
     List<User> findAllByUsername(String username);
